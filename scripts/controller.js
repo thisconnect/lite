19462834479c323ff91d323c53775896bc0f7b86
tcme <hi@this-connect.me>
2012-04-22 01:09:30 +0200

    moving...

---
+++ scripts/controller.js
@@ -0,0 +1,59 @@
+(function(){
+
+var Controller = this.Controller = new Class({
+
+	Implements: [Events, Bound],
+
+	$enabled: true,
+
+	initialize: function(type, data){
+		type = type.capitalize();
+		return Controller[type] ? new Controller[type](data) : this;
+	},
+
+	build: function(){
+		var element = this.element = new Element('div', {
+			'class': 'controller'
+		});
+		this.curtain = new Element('div', {
+			'class': 'curtain',
+			events: {
+				mousedown: function(e){
+					e.stopPropagation();
+				}
+			}
+		}).inject(element);
+		return this;
+	},
+
+	isEnabled: function(){
+		return !!this.$enabled;
+	},
+
+	enable: function(){
+		this.$enabled = true;
+		return this;
+	},
+
+	disable: function(){
+		this.$enabled = false;
+		return this;
+	},
+
+	attach: function(container, position){
+		this.element.inject(container || document.body, position || 'top');
+		return this;
+	},
+
+	detach: function(){
+		this.element.dispose();
+		return this;
+	},
+
+	set: function(value){
+		return this;
+	}
+
+});
+
+})();
--- scripts/ui.js
+++ scripts/ui.js
@@ -61,7 +61,7 @@ var Widget = this.Widget = new Class({
 	},
 
 	attach: function(element, position){
-		this.element.inject(element || document.body, position || 'top');
+		this.element.inject(element || document.body, position || 'bottom');
 		return this;
 	},
 
@@ -80,62 +80,6 @@ var Widget = this.Widget = new Class({
 
 	onStateUpdate: function(value){
 		this.set(value);
-	}
-
-});
-
-var Controller = this.Controller = new Class({
-
-	Implements: [Events, Bound],
-
-	$enabled: true,
-
-	initialize: function(type, data){
-		type = type.capitalize();
-		return Controller[type] ? new Controller[type](data) : this;
-	},
-
-	build: function(){
-		var element = this.element = new Element('div', {
-			'class': 'controller'
-		});
-		this.curtain = new Element('div', {
-			'class': 'curtain',
-			events: {
-				mousedown: function(e){
-					e.stopPropagation();
-				}
-			}
-		}).inject(element);
-		return this;
-	},
-
-	isEnabled: function(){
-		return !!this.$enabled;
-	},
-
-	enable: function(){
-		this.$enabled = true;
-		return this;
-	},
-
-	disable: function(){
-		this.$enabled = false;
-		return this;
-	},
-
-	attach: function(container, position){
-		this.element.inject(container || document.body, position || 'top');
-		return this;
-	},
-
-	detach: function(){
-		this.element.dispose();
-		return this;
-	},
-
-	set: function(value){
-		return this;
 	}
 
 });
