744685c35013c745d12386a288f8eb221f2226b1
tcme <hi@this-connect.me>
2012-08-03 13:39:22 +0200

    node runner (forgot to add server)

---
+++ server.node.js
@@ -0,0 +1,12 @@
+var http = require('http'),
+	connect = require('connect'),
+	planet = require('planet').Planet;
+
+var app = connect()
+	//.use('/instruments', connect.static(__dirname + '/../tcmelib'))
+	.use(connect.static(__dirname))
+	.use(connect.favicon('./favicon.ico'));
+
+var server = http.createServer(app).listen(8004);
+
+new planet(server).listen();
