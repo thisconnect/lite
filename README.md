4a2926e5c1d5cdaa60752b486b297a6396658973
tcme <hi@this-connect.me>
2012-08-03 13:38:58 +0200

    node runner

--- README.md
+++ README.md
@@ -7,22 +7,22 @@ Setup
 	git submodule update --init --recursive
 
 
-Run (node)
-----------
-
-	var connect = require('connect'),
-		http = require('http');
-	
-	var app = connect()
-		.use(connect.static(__dirname + '/satellite'))
-		.use(connect.favicon('./favicon.ico'));
-	
-	http.createServer(app).listen(8004);
-
-
 Run (python)
 ------------
 
 	python -m SimpleHTTPServer 8004
 
 Browse to http://localhost:8004/
+
+
+Setup (node.js)
+---------------
+
+	npm install connect
+	npm install planet
+
+
+Run (node.js)
+-------------
+
+	node server.node.js
--- scripts/planet.js
+++ scripts/planet.js
@@ -1,27 +1,7 @@
 new Unit({
 
 	readySetup: function(){
-		if (null != window.io) this.connect('//:8999');
-		else this.load('/planet', function(response){
-			response = JSON.decode(response);
-
-			this.load(response.io, function(){
-				this.connect(response.connect);
-			});
-		});
-	},
-
-	load: function(location, callback){
-		new Request({
-			url: location,
-			method: 'get',
-			noCache: true,
-			onSuccess: callback.bind(this),
-			onFailure: function(){
-				console.log('errrrr');
-			}
-		}).send();
-		return this;
+		this.connect();
 	},
 
 	connect: function(uri){
