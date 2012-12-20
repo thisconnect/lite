16fc6702e12ee734c58fc5ca3f613a02f580dae2
tcme <hi@this-connect.me>
2012-12-20 13:58:38 +0100

    changes to new planet api 0.9.x (pass socket io instance manually to the planet)

--- server.node.js
+++ server.node.js
@@ -1,12 +1,26 @@
-var http = require('http'),
+var http    = require('http'),
 	connect = require('connect'),
-	planet = require('planet').Planet;
+	io      = require('socket.io'),
+	planet  = require('planet');
+
 
 var app = connect()
 	//.use('/instruments', connect.static(__dirname + '/../abslib'))
 	.use(connect.static(__dirname))
 	.use(connect.favicon('./favicon.ico'));
 
-var server = http.createServer(app).listen(8004);
+var server = http.createServer(app);
 
-new planet(server).listen();
+var socket = io.listen(server, {
+	'transports': ['websocket'],
+	'flash policy server': false,
+	'log level': 1//,
+	//'browser client': false,
+	//'browser client cache': true,
+	//'browser client minification': true,
+	//'browser client gzip': true
+});
+
+var local = planet(socket, {});
+
+server.listen(8004, 'localhost');
