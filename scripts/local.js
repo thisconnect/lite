2aa3031b856ed8cae9c4c5c8e92f0539cf5a8c45
thisconnect <hi@this-connect.me>
2014-02-17 10:43:02 +0100

    local stuff wip

--- scripts/local.js
+++ scripts/local.js
@@ -7,6 +7,9 @@ new Unit({
 			'socket connect': this.setup,
 			'socket disconnect': this.disconnect,
 			'local connect': this.connect,
+			'local set': this.set,
+			'local remove': this.remove,
+			'local merge': this.merge,
 			'local type connect': this.connectType,
 			'local add': this.add
 		});
@@ -22,9 +25,24 @@ new Unit({
 
 	setup: function(local){
 		this.io = local;
+
+		local.on('set', this.onSet.bind(this));
+		local.on('remove', this.onRemove.bind(this));
+		local.on('merge', this.onMerge.bind(this));
+
 		this.type = local.of('/type');
 		this.publish('local type connect', this.type);
 		this.publish('local connect', this.io);
+	},
+
+	types: {},
+
+	connectType: function(type){
+		var that = this;
+		type.emit('get', function(data){
+			console.log('local type', data);
+			that.types = data;
+		});
 	},
 
 	connect: function(local){
@@ -32,14 +50,38 @@ new Unit({
 		this.publish('widget destroy', 'local');
 		local.emit('get', function(data){
 			console.log('local', data);
-	//		that.publish('widget create', ['local', data]);
+			for (var widget in data){
+				if (!(widget in that.types)) continue;
+				that.publish('widget create', ['local', widget, that.types[widget]]);
+			}
 		});
 	},
 
-	connectType: function(type){
-		type.emit('get', function(data){
-			console.log('local type', data);
-		});
+	set: function(path, value){
+		console.log('local set', path, value);
+		this.io.emit('set', path, value);
+	},
+
+	remove: function(key){
+		console.log('local remove', key);
+		this.io.emit('remove', key);
+	},
+
+	merge: function(data){
+		console.log('local merge', data);
+		this.io.emit('merge', data);
+	},
+
+	onSet: function(key, value){
+		console.log('onSet', key, value);
+	},
+
+	onRemove: function(key){
+		console.log('onRemove', key);
+	},
+
+	onMerge: function(data){
+		console.log('onMerge', data);
 	},
 
 	disconnect: function(){
